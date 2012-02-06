7b1994f1f86f566ebfa514cbfe932ff404466680
alexbenfica <alexbenfica@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-06 12:29:38 +0000

    Little change on configuration screen.
    git-svn-id: https://plugins.svn.wordpress.org/publish-to-schedule/trunk@500949 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- publish-to-schedule.php
+++ publish-to-schedule.php
@@ -1157,9 +1157,9 @@ function pts_options_page(){
 		
 		
 
-		<?php
-		echo '<h3 style="margin-top:20px;">'.__('Did it save you a lot of time?','pts').'</h3>';
+		<?php		
 		if($options['pts_statistics_total_work'] > 3){
+			echo '<h3 style="margin-top:20px;">'.__('Did it save you a lot of time?','pts').'</h3>';
 			if($options['pts_statistics_total_work'] > 20){
 				echo __('Ohh yes... it certainly did!','pts');
 				echo '<br>';
